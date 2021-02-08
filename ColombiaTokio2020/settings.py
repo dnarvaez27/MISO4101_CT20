30a86a0057ec3222576e6d7f7c94b259600206d7
David Narvaez <david@gopreki.com>
2021-02-07 23:05:14 -0500

    static

--- ColombiaTokio2020/settings.py
+++ ColombiaTokio2020/settings.py
@@ -130,14 +130,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # STATIC_URL = '/static/'
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # #location where django collect all static files
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # # location where you will store your static files
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
