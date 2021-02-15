dd859ac1449a0ab31a45a0a9847b858fdd9bb805
tdiaz1 <tj.diaz@uniandes.edu.co>
2021-02-14 23:07:28 -0500

    variables de entorno

--- ColombiaTokio2020/settings.py
+++ ColombiaTokio2020/settings.py
@@ -90,11 +90,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'taniadiaz',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
