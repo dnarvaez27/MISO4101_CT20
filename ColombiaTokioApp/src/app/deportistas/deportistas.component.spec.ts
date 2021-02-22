b582aa2b605a63c684e6b84220851b6d1b645867
JuanR <jd.ruedaa@uniandes.edu.co>
2021-02-21 22:56:11 -0500

    Base con app de angular

---
+++ ColombiaTokioApp/src/app/deportistas/deportistas.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeportistasComponent } from './deportistas.component';
+
+describe('DeportistasComponent', () => {
+  let component: DeportistasComponent;
+  let fixture: ComponentFixture<DeportistasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DeportistasComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DeportistasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ColombiaTokioApp/src/app/deportistas/deportistas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deportistas',
+  templateUrl: './deportistas.component.html',
+  styleUrls: ['./deportistas.component.css']
+})
+export class DeportistasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
